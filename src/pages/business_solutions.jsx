905a2c6899a75050ac42a00459403c69769b977c
sam-lukaa <iamsamuelfak@gmail.com>
2022-04-26 06:11:33 -0400

    updated business page

--- src/pages/business_solutions.jsx
+++ src/pages/business_solutions.jsx
@@ -5,15 +5,18 @@ import {
   Card3,
   Slider,
 } from "../components/business_solutions";
+import Container from "../components/container";
 
 export default function Index() {
   return (
     <div>
       <Hero />
-      <Card1 />
-      <Card2 />
-      <Card3 />
-      <Slider />
+      <Container>
+        <Card1 />
+        <Card2 />
+        <Card3 />
+        <Slider />
+      </Container>
     </div>
   );
 }
